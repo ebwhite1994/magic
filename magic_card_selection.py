688048e934a8f6acec75144120d345a1a72c0ca7
Ethan White <ebwhite1994@gmail.com>
2025-08-13 19:47:24 -0400

    adding functions

--- magic_card_selection.py
+++ magic_card_selection.py
@@ -1,2 +1,15 @@
-import numpy as np
-test = np.zeros(10)
+import pandas as pd
+green = pd.read_csv('c:/Users/Moose/OneDrive/Documents/magic_project/Green_Binder.csv')
+
+# class cards:
+#     def __init__(self, min_price, max_price, quantity):
+#         self.min_price = min_price
+#         self.max_price = max_price
+#         self.quantity = quantity
+
+def selection(path, min_price, max_price, quantity):
+    cards = pd.read_csv(path, usecols=['Name', 'Set code', 'Purchase price', 'Quantity'])
+    return(cards)
+    
+print("file has run")
+
