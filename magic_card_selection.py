6a5feecdf092a9e01ea6a7fda99fd61f400f156e
Ethan White <ebwhite1994@gmail.com>
2025-08-13 20:09:35 -0400

    Updated selection function

--- magic_card_selection.py
+++ magic_card_selection.py
@@ -1,15 +1,15 @@
 import pandas as pd
 green = pd.read_csv('c:/Users/Moose/OneDrive/Documents/magic_project/Green_Binder.csv')
-
+test_path = 'c:/Users/Moose/OneDrive/Documents/magic_project/Green_Binder.csv'
 # class cards:
 #     def __init__(self, min_price, max_price, quantity):
 #         self.min_price = min_price
 #         self.max_price = max_price
 #         self.quantity = quantity
 
-def selection(path, min_price, max_price, quantity):
+def selection(path, min_price, max_price, min_quantity):
     cards = pd.read_csv(path, usecols=['Name', 'Set code', 'Purchase price', 'Quantity'])
+    cards = cards[(cards['Purchase price'] > min_price) & (cards['Purchase price'] < max_price) & (cards['Quantity'] >= min_quantity)]
     return(cards)
     
-print("file has run")
 
