49c94566e8bdecb303b7187ddd935586eb8abae0
Logan D.G. Smith <logandawsonsmith@gmail.com>
2025-08-16 00:23:35 -0400

    Generalize magic_card_selection.py a bit

--- magic_card_selection.py
+++ magic_card_selection.py
@@ -1,26 +1,30 @@
+#!/usr/bin/env python3
+
+# STL
+import argparse
+from pathlib import Path
+
+# Third Party Imports
 import pandas as pd
-green = pd.read_csv('c:/Users/Moose/OneDrive/Documents/magic_project/Green_Binder.csv')
-test_path = 'c:/Users/Moose/OneDrive/Documents/magic_project/Green_Binder.csv'
 
-def selection(path, min_price, max_price, min_quantity):
-    cards = pd.read_csv(path, usecols=['Name', 'Set code', 'Purchase price', 'Quantity'])
-    cards = cards[(cards['Purchase price'] > min_price) & (cards['Purchase price'] < max_price) & (cards['Quantity'] >= min_quantity)]
-    return(cards)
+def get_decklist_from_collection(collection:pd.DataFrame, decklist_path:Path) -> (pd.DataFrame, pd.DataFrame):
+    """
+    Find if cards in a decklist are in your collection or not.
 
+    Parameters
+    ----------
+    collection : pd.DataFrame
+        the collection we're checking against
+    decklist : Path
+        path to the decklist CSV that we're interested in checking
 
-#Function to check if cards in a decklist are in your collection or not
-#takes in the path to a collection and the path to a decklist
-#outputs a list of cards in that collection and a list of cards outside the collection
-###
-# The way Moxfield outputs a decklist puts the set code in parenthesis next to the card name.
-# We will probably need to put a filter on the decklist such that it cuts off each string when it reaches a "("
-# and then remove off the end [:-1] to get rid of the extra space. 
-###
-# Something to add would be to check if the exact printing the decklist listed was in your collection instead of just
-# any version of the card. 
-def decklist_match(collection, decklist):
-    #get the names of the cards in the collection
-    collection = pd.read_csv(collection)
+    Returns
+    -------
+    (pd.DataFrame, pd.DataFrame)
+        tuple containing a DataFrame with the cards that are in your collection and a DataFrame
+    including the cards that are not in your collection.
+    """
+    # Get names of cards in collection
     names = collection['Name']
     #Depending on how the decklist is set up, this would need to change. 
     #Moxfield doesn't have a header like our collection files do, so header = None
@@ -43,3 +47,74 @@ def decklist_match(collection, decklist):
         #Maybe yellow highlight for have the card but not the printing in the decklist file. 
         #Option to turn that on/off for people who don't care about the exact art. 
     return(cards_in_collection, cards_not_in)
+
+def select_by_price_example(collection:pd.DataFrame, min_price:float=None, max_price:float=None, min_quantity:float=None) -> pd.DataFrame:
+    """
+    Select cards in collection within a given price range.
+
+    Parameters
+    ----------
+    collection : pd.DataFrame
+        collection you'd like to interact with
+    min_price : float,optional
+        the minimum price allowed in the selection
+    max_price : float,optional
+        the maximum price allowed in the selection
+    min_quantity : float,optional
+        the minimum quantity allowed in the selection
+
+    Returns
+    -------
+    pd.DataFrame
+        filtered card information
+    """
+    # TODO: Actually make these filters optional
+    # TODO: We don't need to define all the filters, we can accept *args and **kwargs and pass them to pandas
+    return [
+        (collection['Purchase price'] >= min_price) &
+        (collection['Purchase price'] <= max_price) &
+        (collection['Quantity'] >= min_quantity)
+    ]
+
+def import_collection_csv(path:Path) -> pd.DataFrame:
+    """
+    Attempts to load a CSV from a path.
+
+    Parameters
+    ----------
+    path : Path
+        the path to your collection CSV that you'd like to import
+
+    Returns
+    -------
+    pd.Data
+        card information from CSV
+    """
+    if not path:
+        return None
+
+    collection = pd.read_csv(path)
+
+    # TODO: Add any parsing / validations you want on collection
+
+    return collection
+
+def main():
+    # Setup argument parser
+    parser = argparse.ArgumentParser(
+        description='Utilities for managing your Magic: the Gathering Collection'
+    )
+
+    parser.add_argument('filename', type=Path, help='Path to the collection CSV you want to import')
+    parser.add_argument('--print', '-p', action='store_true', help='Print the imported CSV to the terminal')
+
+    args = parser.parse_args()
+
+    # Import collection
+    collection = pd.read_csv(args.filename)
+
+    if args.print:
+        print(collection)
+
+if __name__ == '__main__':
+    main()
