d930bcb9a04e119d1cc3570ebbfa15be524c990d
Ethan White <ebwhite1994@gmail.com>
2025-08-25 21:21:04 -0400

    Updated the get_decklist function. Now accurately says what is/isn't in collection for a list of names and for a moxfield file.

--- decklist_test.py
+++ decklist_test.py
@@ -43,10 +43,13 @@ def get_decklist_from_collection(collection:pd.DataFrame, decklist_path:Path) ->
         # Create the mask which is applied to the collection
         # True == the card in the decklist is in the collection
         mask = names.str.contains(pattern, case = False, regex = True)
-        # Apply the mask to collection names to get names of cards in the collection.
-        cards_in_collection = names[mask]
-        # Apply the opposite of the mask to the decklist to get the names of cards you still need.
-        cards_not_in = decklist[~mask]
+        # Apply the mask to collection to get cards in the collection.
+        cards_in_collection = collection[mask]
+        # Determine which cards in the decklist aren't in the collection
+        pattern2 = '|'.join(cards_in_collection)
+        mask2 = decklist.str.contains(pattern2, case = False, regex = True)
+        # Apply the opposite of the mask to determine what isn't in the collection
+        cards_not_in = decklist[~mask2]
         # Output the results
         return(cards_in_collection, cards_not_in)
     # We will need to add more if statements or call other functions once we have more input types (TCGPlayer, TappedOut, etc.)
@@ -62,20 +65,46 @@ def get_decklist_from_collection(collection:pd.DataFrame, decklist_path:Path) ->
         decklist[['Name', "Set Code"]] = decklist['Name'].str.split('(', expand = True)
         # Split the Dataframe by the ) around the set code
         # Splits it such that now we have set code and card ID (number of the card in the set) in their own columns
-        decklist[['Set Code','ID']] = decklist['Set Code'].str.split(')', expand = True)
+        decklist[['Set Code','Collector Number']] = decklist['Set Code'].str.split(')', expand = True)
         # The way we have split these columns, there is an excess space " " in a few columns
         # ex. "Swamp ", "EOE", " 221"
         # Need to remove the excess spaces so they can match up with the labels in the database/dataframe
         decklist['Name'] = decklist['Name'].str[:-1]
-        decklist['ID'] = decklist["ID"].str[1:]
+        decklist['ID'] = decklist["Collector Number"].str[1:]
         # Adjust the Dataframe such that it only uses the columns we have defined
-        decklist = decklist[['Quantity', "Name", 'Set Code', "ID"]]
+        decklist = decklist[['Quantity', "Name", 'Set Code', "Collector Number"]]
         print(decklist["Name"][0])
         pattern = '|'.join(decklist['Name'])
         mask = names.str.contains(pattern, case = False, regex = True)
         cards_in_collection = names[mask]
-        cards_not_in = decklist["Name"][~mask]
-        # cards_not_in = names[~mask]
+        # Drop duplicates for determing if cards are in the collection at all
+        # We will deal with duplicates and specific printings later. 
+        cards_in_collection_single = cards_in_collection.drop_duplicates()
+        print(cards_in_collection_single)
+        # You now have which cards are in the collection, but we also want to output the cards we still need
+        # Create a searchable list of cards in the collection
+        pattern2 = '|'.join(cards_in_collection)
+        print(pattern2)
+        # Create a mask to the decklist 
+        mask2 = decklist["Name"].str.contains(pattern2, case = False, regex = True)
+        cards_not_in = decklist["Name"][~mask2]
+        
+        # If we're just interested in the fact that there are copies of the cards in the collection,
+        # then cards_in_collection and cards_not_in will cover it. 
+        # If we want to see if the exact printing and/or number are in the collection, we need to go further. 
+
+        # We also want to know if the quantity of cards is the same 
+        # In otherwords, you might have a card, but the decklist calls for four of them. 
+        # You might want to know if you have more than one copy too even if the deck only calls for one. 
+        # To that same idea, we can check Set Code and Collector Number 
+        
+        #Get the quantity of cards, considering only cards we own in the decklist
+        # card_quant = collection['Quantity'][mask]
+        # return(collection, decklist, mask, mask2)
+
+        # Determine if the exact printing is in the collection
+        
+
         return(cards_in_collection, cards_not_in)
         
     # #Output the results. 
